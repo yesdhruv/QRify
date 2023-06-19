0e17ef0236648ccfaf42525387d28536a72daef6
Dhruv Rawat <dhruvrawat@Dhruvs-MacBook-Air.local>
2023-06-19 19:39:33 +0530

    added

---
+++ script.js
@@ -0,0 +1,37 @@
+const wrapper = document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn = wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+// Add audio of your choice 
+const audio = document.getElementById("audio");
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+
+
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+
+        audio.currentTime = 0; // Reset the audio to the beginning
+        audio.play();
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
+
+
+
+
+
+
+
